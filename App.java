687c7f25a19d1e73901a6cda10de778d88c67e45
marie-claire-yui <marieclairekasai@gmail.com>
2023-05-04 16:11:37 +0200

    a

--- App.java
+++ App.java
@@ -1,23 +1,35 @@
 // portee - constuire un  objet comme étant une classe - nom de la classe identique au nom du fichier
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args){
        // System.out.println("Salut les devs");
      //   Miaou tom = new Miaou(); // instance de la classe Miaou
      //   Miaou tom2 = new Miaou(); // chaque  instance de chat aura une allocation mémoire, un espace qui lui est alloué, il existe un réfèrencement pour le retrouver 
-    Miaou c = new Miaou("Tom",1,false);
-    Miaou c2 = new Miaou("Félix",2,true);
+    Miaou c = new Miaou("Tom",1,false,"miaou");
+    Miaou c2 = new Miaou("Félix",2,true,"miaou");
   //  Ouaf d = new Ouaf("Snoopy",12,false)
        // Ouaf jingle =  new Ouaf();
         name(c);
-        name(c2);
+      name(c2);
 
     }
     public static void name(Miaou chat) {
     //    System.out.println("c.mAge" + chat.getAge());
     //    System.out.println("c.mName" + chat.getName());  
-    //    System.out.println("c.mName" + chat.getVaccin());  
-    System.out.println("Bonjour je m'appelle: " +chat.getAge()+ " je suis un chat de : "+chat.getAge()+ " et " +(chat.getVaccin()?"je suis vacciné":"je ne suis pas vacciné"));     
+    //    System.out.println("c.mName" + chat.getVaccin()); 
+     Scanner scanner = new Scanner( System.in ); 
+    System.out.println("Bonjour je m'appelle: " +chat.getName()+ " je suis un chat de : "+chat.getAge()+ " ans et " +(chat.getVaccin()?"je suis vacciné":"je ne suis pas vacciné")+" Voulez-vous que je miaule?");     
+    String reponse = scanner.nextLine();
+    if (reponse.equals("oui")){
+        System.out.println(chat.getAction());
+    }else {
+        System.out.println("ronron");
     }
+  //   scanner.close();
+
+}
 
  
 }
--- Miaou.java
+++ Miaou.java
@@ -2,11 +2,14 @@ public class Miaou {
     private String mName;
     private int mAge;
     private boolean mVaccin;
-    public Miaou(String name, int age, boolean vaccin){
+    private String mAction;
+
+    public Miaou(String name, int age, boolean vaccin, String action){
        // System.out.println("Je suis le chat" + this); // this donne la "référence de l'instance" dans la console
         this.mName = name;
         this.mAge = age;
         this.mVaccin = vaccin;
+        this.mAction = action;
        // System.out.println("Bonjour je m'appelle : " + this.mName + " je suis un chat de " + this.mAge + " ans, vaccin : " + this.mVaccin);
     //   System.out.println("Bonjour je m'appelle : " + this.mName + " je suis un chat de " + this.mAge + " ans" +  (this.mVaccin ? " Je suis vacciné" : " Je ne suis pas vacciné"));
     }
@@ -20,5 +23,9 @@ public class Miaou {
 
  public boolean getVaccin(){
     return mVaccin;
+ }
+
+ public String getAction(){
+    return mAction;
  }
 }
